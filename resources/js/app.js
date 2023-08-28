b73f26ed611e9e61f77394512e28624637df826a
HAMAD ABDULLA <bo3bdo@hotmail.com>
2023-08-28 15:56:59 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,47 +9,47 @@ Alpine.plugin(focus);
 
 Alpine.start();
 
-
-let blocks = document.querySelectorAll("pre");
-let copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
-    '  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />\n' +
-    '</svg>';
-let copiedIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>';
-
-
-blocks.forEach((block) => {
-    if (!navigator.clipboard) {
-        return;
-    }
-
-    let button = document.createElement("button");
-    button.className = "button-copy-code";
-
-    button.innerHTML = copyIcon;
-    block.appendChild(button);
-
-    button.addEventListener("click", async () => {
-        await copyCode(block);
-    });
-});
-
-async function copyCode(block) {
-    let copiedCode = block.cloneNode(true);
-    copiedCode.removeChild(copiedCode.querySelector("button.button-copy-code"));
-
-    const html = copiedCode.outerHTML.replace(/<[^>]*>?/gm, "");
-
-    block.querySelector("button.button-copy-code").innerHTML = copiedIcon;
-    setTimeout(function () {
-        block.querySelector("button.button-copy-code").innerHTML = copyIcon;
-    }, 2000);
-
-    const parsedHTML = htmlDecode(html);
-
-    await navigator.clipboard.writeText(parsedHTML);
-}
-
-function htmlDecode(input) {
-    const doc = new DOMParser().parseFromString(input, "text/html");
-    return doc.documentElement.textContent;
-}
+//
+// let blocks = document.querySelectorAll("pre");
+// let copyIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
+//     '  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z" />\n' +
+//     '</svg>';
+// let copiedIcon = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>';
+//
+//
+// blocks.forEach((block) => {
+//     if (!navigator.clipboard) {
+//         return;
+//     }
+//
+//     let button = document.createElement("button");
+//     button.className = "button-copy-code";
+//
+//     button.innerHTML = copyIcon;
+//     block.appendChild(button);
+//
+//     button.addEventListener("click", async () => {
+//         await copyCode(block);
+//     });
+// });
+//
+// async function copyCode(block) {
+//     let copiedCode = block.cloneNode(true);
+//     copiedCode.removeChild(copiedCode.querySelector("button.button-copy-code"));
+//
+//     const html = copiedCode.outerHTML.replace(/<[^>]*>?/gm, "");
+//
+//     block.querySelector("button.button-copy-code").innerHTML = copiedIcon;
+//     setTimeout(function () {
+//         block.querySelector("button.button-copy-code").innerHTML = copyIcon;
+//     }, 2000);
+//
+//     const parsedHTML = htmlDecode(html);
+//
+//     await navigator.clipboard.writeText(parsedHTML);
+// }
+//
+// function htmlDecode(input) {
+//     const doc = new DOMParser().parseFromString(input, "text/html");
+//     return doc.documentElement.textContent;
+// }
