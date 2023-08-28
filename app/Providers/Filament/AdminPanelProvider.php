6ed3e501c06aea26759ab1fdf90075d5db7a02ee
HAMAD ABDULLA <bo3bdo@hotmail.com>
2023-08-28 15:15:43 +0300

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('bo3bdo')
             ->login()
             ->colors([
                 'primary' => Color::Yellow,
